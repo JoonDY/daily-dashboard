9c528e6e24633bd72814b82edfba6a32e414d69c
Joon <daniel.joon.yi@gmail.com>
2021-09-14 14:10:59 -0700

    Changes

--- js/activity.js
+++ js/activity.js
@@ -1,9 +1,7 @@
-export const getActivity = async() => {
+export const getActivity = async () => {
   const url = 'http://www.boredapi.com/api/activity/';
   const bored = document.getElementById('bored');
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data.activity);
   bored.textContent = data.activity;
-
 };
--- js/weather.js
+++ js/weather.js
@@ -1,43 +1,44 @@
-import { API_KEY_WEATHER } from "./key.js";
+import { API_KEY_WEATHER } from './key.js';
 
-export const getLocation = (zip, unit='imperial') => {
+export const getLocation = (zip, unit = 'imperial') => {
   let long;
   let lat;
   const units = unit;
 
-  if  (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
       const API = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY_WEATHER}&units=${units}`;
       getWeather(API);
-    })} else {
-      const API = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${API_KEY_WEATHER}&units=${units}`;
-      getWeather(API);
-    }
+    });
+  } else {
+    const API = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${API_KEY_WEATHER}&units=${units}`;
+    getWeather(API);
   }
+};
 
-
-const getWeather = async(url) => {
+const getWeather = async (url) => {
   const res = await fetch(url);
   const data = await res.json();
   createWeather(data);
-}
+};
 
 const createWeather = (data) => {
+  console.log(data);
   const weather = document.getElementById('weather');
   const div = document.createElement('div');
-  const currentTemp = document.createElement('p')
-  const futureTemps = document.createElement('div')
+  const currentTemp = document.createElement('p');
+  const futureTemps = document.createElement('div');
   futureTemps.classList.add('flex');
   const minTemp = document.createElement('p');
   const maxTemp = document.createElement('p');
   const humidity = document.createElement('p');
 
   humidity.textContent = `Humidity: ${Math.floor(data.main.humidity)}%`;
-  maxTemp.textContent = `H: ${Math.floor(data.main.temp_max)}°`;
-  minTemp.textContent = `L: ${Math.floor(data.main.temp_min)}°`;
+  // maxTemp.textContent = `H: ${Math.floor(data.main.temp_max)}°`;
+  // minTemp.textContent = `L: ${Math.floor(data.main.temp_min)}°`;
   currentTemp.textContent = `${Math.floor(data.main.temp)}°`;
 
   futureTemps.append(maxTemp);
@@ -48,4 +49,4 @@ const createWeather = (data) => {
   div.append(humidity);
 
   weather.append(div);
-}
+};
