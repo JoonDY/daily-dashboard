041af173a64204b8e98cbe6f3cd60ddad136e2cf
Joon <daniel.joon.yi@gmail.com>
2021-09-14 21:59:30 -0700

    Changes

--- js/stocks.js
+++ js/stocks.js
@@ -1,8 +1,8 @@
-import { API_KEY_STOCKS } from "./key.js";
+import { API_KEY_STOCKS } from './key.js';
 
-export const getStocks = async() => {
+export const getStocks = async () => {
   const stocks = document.getElementById('stocks');
-  const url = `https://api.twelvedata.com/time_series?symbol=SPX&interval=1day&apikey=${API_KEY_STOCKS}`
+  const url = `https://api.twelvedata.com/time_series?symbol=SPX&interval=1day&apikey=${process.env.API_KEY_STOCKS}`;
   const res = await fetch(url);
   const data = await res.json();
   createStock(data);
@@ -12,23 +12,22 @@ const createStock = (data) => {
   const currentStock = calculateStock(data);
   const div = document.createElement('div');
   const h2 = document.createElement('h2');
-  h2.classList.add('stock-text')
-
+  h2.classList.add('stock-text');
 
   if (currentStock > 0) {
-    h2.classList.add('text-green')
+    h2.classList.add('text-green');
   } else if (currentStock < 0) {
-    h2.classList.add('text-red')
-  };
+    h2.classList.add('text-red');
+  }
 
   h2.textContent = `${currentStock}%`;
   div.append(h2);
-  stocks.append(div)
+  stocks.append(div);
 };
 
 const calculateStock = (data) => {
   const open = data.values[0].open;
   const close = data.values[0].close;
 
-  return Math.round((((close - open) / open) * 100) * 100) / 100 
+  return Math.round(((close - open) / open) * 100 * 100) / 100;
 };
